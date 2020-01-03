4969f9630b49b0a1a8b9afd9a6c13f7fe91f30e5
Travis Weir <tweirtx@gmail.com>
2020-01-03 13:31:53 -0600

    Wrong variable name

--- openapi_server/db.py
+++ openapi_server/db.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker, Session
 
 __all__ = ['RulesDB', 'Session']
 
-engine = sqlalchemy.create_engine(os.getenv('DB_URL'))
+engine = sqlalchemy.create_engine(os.getenv('DATABASE_URL'))
 DatabaseObject = declarative_base(bind=engine, name='DatabaseObject')
 DatabaseObject.__table_args__ = {'extend_existing': True}
 
